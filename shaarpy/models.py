67c9c08f7766d3cf581f315608705809cb35ef71
FoxMaSk <foxmask@protonmail.com>
2022-07-28 22:08:38 +0200

    validator for latin and 한글 chars

--- shaarpy/models.py
+++ shaarpy/models.py
@@ -2,12 +2,18 @@
 """
     ShaarPy :: Models
 """
+from django.core.validators import RegexValidator
 from django.db import models
 from django.utils import timezone
 
+# Regex to match latin and 한글 (hangul) char
+alphanum_hangul = RegexValidator(
+    r'^[0-9a-zA-Z\u3131-\uD79D\u1100-\u11FF\u3130-\u318F\uA960-\uA97F\uAC00-\uD7AF\uD7B0-\uD7FF,]*$',
+    'Only alphanumeric characters are allowed.')
+
 
 class Links(models.Model):
-    tags = models.CharField(max_length=255, null=True, blank=True)
+    tags = models.CharField(max_length=255, null=True, blank=True, validators=[alphanum_hangul])
     url = models.URLField(max_length=2048, null=True, blank=True)
     url_hashed = models.SlugField(max_length=10, null=True, blank=True)
     title = models.CharField(max_length=255, null=True, blank=True)
